db9890d9d93215efb8aefd70e24c3d26ef845328
Sam Liu <ontouchstart@gmail.com>
2019-10-16 12:00:59 -0400

    docs/mocak-spike-150.html

---
+++ bin/donation_qr.js
@@ -0,0 +1,27 @@
+const fs = require('fs')
+const QRCode = require('qrcode')
+const axios = require('axios')
+const data = 'https://www.mocaspike150.org/data/ambassadors.json'
+const dir = 'docs/ambassadors-slideshow'
+if (!fs.existsSync(dir)){
+    fs.mkdirSync(dir, { recursive: true });
+}
+
+const qr = (path, fn) => {
+  QRCode.toDataURL(path, function (err, url) {
+    const html = `
+      <img width="100%" src="${url}"/>
+      `
+    fs.writeFile(fn, html, (err) => {
+      if(err) {
+        console.log(err)
+      }
+      else {
+        console.log(fn)
+      }
+    })
+  })
+}
+
+const url = 'https://www.crowdrise.com/o/en/campaign/moca-spike-150'
+qr(url, 'docs/mocak-spike-150.html')
